0e810d52bb7b1aebfd239af106923e8ebbc91a5a
Mahmoud Heeba <mhmwdhybh585@gmail.com>
2026-08-12 18:31:33 +0300

    Theoretical Questions

--- Assignment OOP 02/Program.cs
+++ Assignment OOP 02/Program.cs
@@ -7,6 +7,16 @@ namespace Assignment_OOP_02
         static void Main(string[] args)
         {
 
+            #region Theoretical Questions
+            //Q1:
+            Console.WriteLine("a.Abstraction: Hiding complixity details with showing princible details ");
+            Console.WriteLine("b. reduse complixity & promote filixabilty ");
+            // Q2:
+            Console.WriteLine("a. abstract contain fields , methods ,allow single inhiritance but interface contain contract(signture methods) , doesn't contain fields & allow multiple implemention ) " +
+                "b. if there multiple implementation i use interface " +
+                "c.no,yes");
+            #endregion
+
 
         }
     }
