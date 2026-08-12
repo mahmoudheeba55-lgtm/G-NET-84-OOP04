6d40a5a13068cb0eb92691806b00a6bcf95932b9
Mahmoud Heeba <mhmwdhybh585@gmail.com>
2026-08-12 18:35:04 +0300

    Practical Questions

--- Assignment OOP 02/Program.cs
+++ Assignment OOP 02/Program.cs
@@ -16,12 +16,63 @@ namespace Assignment_OOP_02
                 "b. if there multiple implementation i use interface " +
                 "c.no,yes");
             #endregion
+            #region  Practical
+
+            Driver driver = new Driver(123, "Ali", 0125565698);
+            DeliveryCenter center = new DeliveryCenter("Amazon");
+            DeliveryAdress deliveryAddress = new DeliveryAdress("elmahala", "elshone", 6);
+            center.driver = driver;
+            StandardShipment standardShipment = new StandardShipment("sh001", "laptob", 3, 80, deliveryAddress);
+            Console.WriteLine("Enter Extra Fee:");
+            decimal ExtraFee = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Extra Fee: {ExtraFee}");
+            ExpressShipment expressShipment = new ExpressShipment("sh002", "mobile ", 2, 60, deliveryAddress, ExtraFee);
+            Console.WriteLine("Enter your Destination country:");
+            string country = Console.ReadLine();
+            InternationalShipment international = new InternationalShipment("sh003", "television", 8, 120, deliveryAddress, country, 300);
+            ITrackable[] trackables = new ITrackable[] { standardShipment, expressShipment, international };
+            ITrackable[] iinsurable = new ITrackable[] { standardShipment, expressShipment, international };
+
+            center.AddShipment(standardShipment);
+            center.AddShipment(expressShipment);
+            center.AddShipment(international);
+
+            Shipment[] printTypeshipment = new Shipment[] { standardShipment, expressShipment, international };
+            for (int i = 0; i < 3; i++)
+            {
+                printTypeshipment[i].PrintShipment();
+            }
+            Console.WriteLine("==========================================");
+
+            Console.WriteLine("Tracking Status");
 
 
+            string print = standardShipment.GetTrackingStatus();
+            Console.WriteLine(print);
+            string show = expressShipment.GetTrackingStatus();
+            Console.WriteLine(show);
+            string _show = international.GetTrackingStatus();
+            Console.WriteLine(_show);
+
+            Console.WriteLine("========================================");
+
+            Console.WriteLine("Insurance");
+            decimal x = standardShipment.CalculateInsurance();
+            Console.WriteLine(x);
+            decimal y = expressShipment.CalculateInsurance();
+            Console.WriteLine(y);
+            decimal z = international.CalculateInsurance();
+            Console.WriteLine(z);
+
+            Console.WriteLine("========================================");
+            Console.WriteLine("Interface Polymorphism Demonstrated Successfully.");
+            #endregion
         }
+
+    }
     }
 
-}
+
 
 
 
